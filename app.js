18de10633c011f38f9366891d91da17fef0d76b7
xvargr <targenta650@gmail.com>
2022-05-10 20:18:54 +0800

    EOD upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// npm init
+
 const express = require("express"); //import the express npm module for easy server setup
 const app = express(); //mapping express to app
 const path = require("path"); //import path npm module to work with folder structure easier
@@ -5,9 +7,13 @@ const path = require("path"); //import path npm module to work with folder struc
 app.set("view engine", "ejs"); //set views engine to ejs
 app.set("views", path.join(__dirname, "views")); //set views directory to a dynamic directory
 
+const formMethod = require("method-override"); //gives forms access to more methods
+app.use(formMethod("_method"));
+
 const mongoose = require("mongoose"); //import mongoose module to work with mongo.db from js
 
 const Library = require("./models/libraries"); //import library model
+const libraries = require("./models/libraries");
 
 mongoose.connect("mongodb://localhost:27017/libraries", {
   useNewUrlParser: true,
@@ -25,20 +31,51 @@ db.once("open", function () {
 
 const port = 3000; //set listening port to this
 
+app.use(express.urlencoded({ extended: true })); //express middleware body parser
+
 app.listen(port, function () {
   console.log("---> App started");
   console.log(`---> Listening on port ${port}`);
 }); //express start server
 
-app.get("/", function (req, res) {
-  res.render("home");
+app.get("/libraries", async function (req, res) {
+  const result = await Library.find({});
+  res.render("libraries/index", { result });
 }); //run this when receiving get request to "/"
 
-app.get("/newlibrary", async function (req, res) {
-  const lib = new Library({
-    name: "Test Library",
-    description: "This is a test library",
-  });
-  await lib.save(); //wait for this to finish
-  res.send(lib);
-}); //create async function and create a new test library
+//; if get /:id is placed before /new, express will try to
+//try to search and match a details page with the id of "new".
+//place get /new first to solve this issue
+
+app.get("/libraries/new", function (req, res) {
+  res.render("libraries/new");
+}); //new form route for to create libraries
+
+app.get("/libraries/:id", async function (req, res) {
+  const { id } = req.params; //destructure req.params to get id
+  const result = await Library.findById(id);
+  res.render("libraries/details", { result });
+}); //details route for specific libraries
+
+//    ↓↓↓↓↓ CANNOT GET! ↓↓↓↓↓
+app.get("libraries/:id/edit", async function (req, res) {
+  const { id } = req.params;
+  const result = await Library.findById(id);
+  res.render("libraries/edit", { result });
+}); //serve edit form
+
+app.post("/libraries", async function (req, res) {
+  //res.send(req.body); //by default, req.body is empty, it needs to be parsed
+  const newLib = new Library(req.body.newLib);
+  await newLib.save();
+  res.redirect(`/libraries/${newLib._id}`);
+});
+
+// app.get("/newlibrary", async function (req, res) {
+//   const lib = new Library({
+//     name: "Test Library",
+//     description: "This is a test library",
+//   });
+//   await lib.save(); //wait for this to finish
+//   res.send(lib);
+// }); //create async function and create a new test library
