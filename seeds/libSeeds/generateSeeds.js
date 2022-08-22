09f5f8a52c98dbf388212bf53f3b2326ee1febde
xvargr <targenta650@gmail.com>
2022-08-22 18:28:43 +0800

    mobule layout zise fix, vhanged name from bits to pins

--- seeds/libSeeds/generateSeeds.js
+++ seeds/libSeeds/generateSeeds.js
@@ -11,10 +11,13 @@ const ExpressError = require("../../utils/ExpressError");
 const size = 50;
 const maxPrice = 100;
 
-mongoose.connect("mongodb://localhost:27017/libraries", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}); //connect mongoose to mongodb at this directory
+mongoose.connect(
+  "mongodb+srv://admin:wFzqjfTrJlvLcELE@cluster0.utulezr.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+); //connect mongoose to mongodb at this directory
 
 const db = mongoose.connection; //assign db shorthand to mongoose.connection
 db.on(
